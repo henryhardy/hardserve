b1687026ab8a2e2a6b7cf7cd2a6908a48825d9bf
Henry Hardy <henry@digitalassetgroup.com.au>
2017-04-10 23:04:25 +1000

    bump

---
+++ hardserve.ino
@@ -0,0 +1,17 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
